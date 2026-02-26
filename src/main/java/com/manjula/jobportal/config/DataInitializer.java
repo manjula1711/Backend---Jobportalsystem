1bd67843aae1996ca72afc7f2aa2fca69bad4ef7
manjula1711 <141096624+manjula1711@users.noreply.github.com>
2026-02-26 23:22:08 +0530

    Create DataInitializer.java

---
+++ src/main/java/com/manjula/jobportal/config/DataInitializer.java
@@ -0,0 +1,36 @@
+package com.manjula.jobportal.config;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.manjula.jobportal.repository.UserRepository;
+import com.manjula.jobportal.model.User;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Component
+public class DataInitializer implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public void run(String... args) {
+
+        if (userRepository.findByEmail("admin@gmail.com").isEmpty()) {
+
+            User admin = new User();
+            admin.setEmail("admin@gmail.com");
+            admin.setPassword(passwordEncoder.encode("admin123"));
+            admin.setRole("ADMIN");
+
+            userRepository.save(admin);
+
+            System.out.println("Admin user created!");
+        }
+    }
+}
