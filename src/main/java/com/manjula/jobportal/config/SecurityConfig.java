8c6b2d8c914890bd9a37c143703a1f063d7fc0a6
manjula1711 <141096624+manjula1711@users.noreply.github.com>
2026-02-26 22:27:37 +0530

    Update SecurityConfig.java

--- src/main/java/com/manjula/jobportal/config/SecurityConfig.java
+++ src/main/java/com/manjula/jobportal/config/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
             .authorizeHttpRequests(auth -> auth
                 // ✅ Allow preflight requests
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                .requestMatchers("/").permitAll()
 
                 .requestMatchers("/api/auth/**").permitAll()
                 .requestMatchers("/api/jobs/**").permitAll()
