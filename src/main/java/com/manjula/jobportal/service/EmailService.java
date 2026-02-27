29ea4d0ee84037f6c870d6af88dd9da386aff79d
manjula1711 <141096624+manjula1711@users.noreply.github.com>
2026-02-27 12:59:52 +0530

    Update EmailService.java

--- src/main/java/com/manjula/jobportal/service/EmailService.java
+++ src/main/java/com/manjula/jobportal/service/EmailService.java
@@ -11,21 +11,24 @@ public class EmailService {
     @Autowired
     private JavaMailSender mailSender;
 
-    public void sendResetPasswordEmail(String toEmail, String resetLink) {
+   public void sendResetPasswordEmail(String toEmail, String resetLink) {
 
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("Job Portal - Password Reset Request");
-        message.setText(
-                "Hi,\n\n" +
-                "We received a request to reset your password.\n\n" +
-                "Click the link below to reset your password:\n" +
-                resetLink + "\n\n" +
-                "This link will expire in 15 minutes.\n\n" +
-                "If you did not request this, please ignore this email.\n\n" +
-                "Thanks,\nJob Portal Team"
-        );
+    SimpleMailMessage message = new SimpleMailMessage();
 
-        mailSender.send(message);
-    }
+    message.setFrom("jobportalsystemapp@gmail.com"); // ADD THIS
+    message.setTo(toEmail);
+    message.setSubject("Job Portal - Password Reset Request");
+
+    message.setText(
+            "Hi,\n\n" +
+            "We received a request to reset your password.\n\n" +
+            "Click the link below to reset your password:\n" +
+            resetLink + "\n\n" +
+            "This link will expire in 15 minutes.\n\n" +
+            "If you did not request this, please ignore this email.\n\n" +
+            "Thanks,\nJob Portal Team"
+    );
+
+    mailSender.send(message);
+}
 }
